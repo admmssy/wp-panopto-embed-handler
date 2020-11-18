2341d47e0efe994ee3edbdfbfda3686d04a58a25
Adam Franco <afranco@middlebury.edu>
2020-11-18 09:42:03 -0500

    Rename podcast file handler for clarity.
    As I was porting this I had gotten confused as to if this was trying to
    handle podcast feeds (a list) verses a single file.

--- panopto-embed-handler.php
+++ panopto-embed-handler.php
@@ -35,9 +35,9 @@ function wp_embed_handler_panopto( $matches, $attr, $url, $rawattr ) {
 	return apply_filters( 'embed_panopto', $embed, $matches, $attr, $url, $rawattr );
 }
 
-wp_embed_register_handler( 'panopto_podcast', '#https?:\/\/(.*)Panopto\/Podcast\/(StreamInBrowser|Download)\/(.*)#i', 'wp_embed_handler_panopto_podcast' );
+wp_embed_register_handler( 'panopto_podcast_file', '#https?:\/\/(.*)Panopto\/Podcast\/(StreamInBrowser|Download)\/(.*)#i', 'wp_embed_handler_panopto_podcast_file' );
 
-function wp_embed_handler_panopto_podcast( $matches, $attr, $url, $rawattr ) {
+function wp_embed_handler_panopto_podcast_file( $matches, $attr, $url, $rawattr ) {
 	$audio = sprintf( '<audio style="visibility:visible;" src="%s" controls="controls">', esc_url( $url ) );
 	return apply_filters( 'wp_embed_handler_audio', $audio, $attr, $url, $rawattr );
 }
